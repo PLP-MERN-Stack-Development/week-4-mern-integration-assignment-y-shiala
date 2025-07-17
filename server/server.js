3f12270afb5b98f83d708bac7301638f3d5360c6
y-shiala <yvanashivon@gmail.com>
2025-07-17 09:20:07 +0300

    Update server.js
    update

--- server/server.js
+++ server/server.js
@@ -16,7 +16,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Middleware
-app.use(cors());
+app.use(cors( {
+  origin: 'http://localhost:5173', 
+  credentials: true,
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
